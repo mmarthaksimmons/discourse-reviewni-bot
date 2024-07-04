25f58567e77d2966d1a7d07ddfa0b5775e0c828c
mmarthaksimmons <mmarthaksimmons@gmail.com>
2024-07-04 14:52:31 +0800

    Update init-search-banner.js

--- javascripts/discourse/api-initializers/init-search-banner.js
+++ javascripts/discourse/api-initializers/init-search-banner.js
@@ -2,7 +2,6 @@ import { apiInitializer } from "discourse/lib/api";
 import SearchBanner from "../components/search-banner";
 import axios from "axios";
 
-
 export default apiInitializer("1.14.0", (api) => {
   api.renderInOutlet(
     settings.plugin_outlet === "above-main-container"
